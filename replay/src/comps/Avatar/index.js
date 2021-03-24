4c4858db963cbbd4e87e654e5f91f4bdc65b2fa9
= <=>
2021-03-23 21:55:45 -0700

    styling profile page, avatar comp

--- replay/src/comps/Avatar/index.js
+++ replay/src/comps/Avatar/index.js
@@ -5,21 +5,16 @@ import styled from 'styled-components';
 
 const InfoContainer = styled.div`
   display: flex;
-  position: relative;
-  justify-content:center;
-  align-items:center;
+  flex-direction:column;
+  align-items:center; 
 `;
 
 const Profile = styled.div`
-position:relative  ;
   width: 145px;
   height: 145px;
   border-radius: 50%;
   background: linear-gradient(180deg, rgba(240, 100, 73, 0.54) 0%, rgba(255, 255, 255, 0) 100%), rgba(194, 181, 181, 0.34);
   z-index: 0;
-  /* background-image: ${(props) =>
-    props.profile ? props.profile : "url('avatar.svg')"}; */
-  /* background-size: 67px 67px; */
   background-image:"url('avatar.svg')";
   display:flex;
   align-items:center;
@@ -27,21 +22,11 @@ position:relative  ;
   background-repeat: no-repeat; 
   object-fit:contain;
   overflow:hidden;
-margin-top:85px;
-& > img {
-  width:auto;
-  height:100%;
-  
-}
-`;
-
-const NameUser = styled.h3`
-  
-  position: absolute;
-  margin-top:300px;
-  justify-content:center;
-  align-items:center;
-`;
+  & > img {
+    width:auto;
+    height:100%;
+    }
+  `;
 
 
 const Avatar = ({ profile, name, onClick }) => {
@@ -50,7 +35,7 @@ const Avatar = ({ profile, name, onClick }) => {
       <Profile>
         <img src={profile} />
       </Profile>
-      {/* <NameUser>{name}</NameUser> */}
+      <h2>{name}</h2>
     </InfoContainer>
   );
 };
